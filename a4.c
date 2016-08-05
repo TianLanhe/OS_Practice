b4ef050eab0ee28465b6d987a33c2e24c074c72e
振宇 <849416763@qq.com>
2016-08-05 16:34:07 +0800

    Update a4.c

--- a4.c
+++ a4.c
@@ -9,17 +9,17 @@
 #define READY 1
 #define CREAT 10
 typedef struct _process{
-	int pid;
-	int priority;
-	int cputime;
-	int needtime;
-	int state;
-	int createtime;
+	int pid;				//进程ID
+	int priority;			//进程优先级
+	int cputime;			//进程已运行的时间
+	int needtime;			//进程距离执行结束还需要运行的时间
+	int state;				//状态：READY,RUN
+	int createtime;			//进程创建的时间，自己添加的，用于计算周转时间等
 }PCB;
 void InitPro(PCB *pcb);
-//初始化进程PCB，pid置0
+//初始化进程PCB，pid置-1表示没有进程占用这块PCB
 Status CreatPro(PCB *pcb,int createtime);
-//创建一个进程，随机赋予其优先权，需要运行时间，并设置其状态为ready
+//挑选一个空的PCB创建一个进程，随机赋予其优先权，需要运行时间，并设置其状态为ready
 void PrintPro(PCB *pcb);
 //打印PCB列表
 Status Iteration(PCB *pcb);
@@ -29,6 +29,7 @@ Status RemovePro(PCB *pcb,int pid);
 int SelectPro(PCB *pcb);
 //选择一个就绪进程中优先级最高的进程，并返回其下标
 int RunPro(PCB *pcb);
+//模拟进程获得CPU运行，
 double Clean(PCB *pcb,int index,int iterator);
 
 void InitPro(PCB *pcb){
@@ -41,15 +42,15 @@ Status CreatPro(PCB *pcb,int createtime){
 	for(i=0;i<100;i++){
 		if(pcb[i].pid == -1){
 			pcb[i].pid=i;
-			pcb[i].priority=rand()%50;
+			pcb[i].priority=rand()%50;		//优先级范围[0,50)
 			pcb[i].cputime=0;
-			pcb[i].needtime=rand()%10+1;
+			pcb[i].needtime=rand()%10+1;	//一共要运行的时间[1,10]
 			pcb[i].createtime=createtime;
 			pcb[i].state=READY;
 			break;
 		}
 	}
-	if(i==100)return ERROR;
+	if(i==100)return ERROR;					//PCB已满，创建失败
 	else return i;
 }
 Status RemovePro(PCB *pcb,int pid){
@@ -100,23 +101,24 @@ int RunPro(PCB *pcb){
 	int index;
 	int temp;
 	int i;
-	if(!Iteration(pcb))return ERROR;
-	index=SelectPro(pcb);
-	pcb[index].priority-=3;
+	if(!Iteration(pcb))return ERROR;		//没有就绪或阻塞进程，挑不出可执行的进程
+	index=SelectPro(pcb);					//取得就绪列表里优先级最高的进程下标
+	//模拟运行，这里干了很多事
+	pcb[index].priority-=3;					//时间片已到，修改进程PCB数据结构
 	if(pcb[index].priority < 0)pcb[index].priority=0;
 	pcb[index].needtime--;
 	pcb[index].cputime++;
-	pcb[index].state=RUN;
-	for(i=0;i<100;i++){
+	pcb[index].state=RUN;					//这里才显示RUN有点不合逻辑，不过为了方便
+	for(i=0;i<100;i++){						//观察就这样吧
 		if(i != index)pcb[i].priority++;
 	}
 	PrintPro(pcb);
 	pcb[index].state=READY;
-	getchar();
+	getchar();								//每次打印后停下来给用户观察
 	return index;
 }
-double Clean(PCB *pcb,int index,int iterator){
-	double average=0;
+double Clean(PCB *pcb,int index,int iterator){	//清除操作，就是撤销掉进程之后顺便
+	double average=0;							//计算一下执行时间，周转时间，平均周转时间等
 	if(!pcb[index].needtime){
 		RemovePro(pcb,index);
 		average=(iterator-pcb[index].createtime)*1.0/pcb[index].cputime;
